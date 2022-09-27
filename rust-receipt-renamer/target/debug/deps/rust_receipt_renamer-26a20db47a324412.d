96cf84b4899eb416f9ef635ff92cbade9b5dcb41
code0716 <job_info@code0716.com>
2022-09-27 19:10:55 +0900

    remove useless files

--- rust-receipt-renamer/target/debug/deps/rust_receipt_renamer-26a20db47a324412.d
+++
@@ -1,7 +0,0 @@
-/Users/toshiyukisugai/learn/rust-study/rust-receipt-renamer/target/debug/deps/rust_receipt_renamer-26a20db47a324412.rmeta: src/main.rs
-
-/Users/toshiyukisugai/learn/rust-study/rust-receipt-renamer/target/debug/deps/rust_receipt_renamer-26a20db47a324412.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::needless_return__CLIPPY_HACKERY__
--- rust-receipt-renamer/target/debug/deps/rust_receipt_renamer-a26daa002c90d883.d
+++
@@ -1,7 +0,0 @@
-/Users/toshiyukisugai/learn/rust-study/rust-receipt-renamer/target/debug/deps/rust_receipt_renamer-a26daa002c90d883.rmeta: src/main.rs
-
-/Users/toshiyukisugai/learn/rust-study/rust-receipt-renamer/target/debug/deps/rust_receipt_renamer-a26daa002c90d883.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::needless_return__CLIPPY_HACKERY__
